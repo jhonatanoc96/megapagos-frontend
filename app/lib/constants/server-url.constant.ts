9cdd5c4f45873f8888ca0e1336a5e102ae8745b1
Jhonatan Ocampo <jhonatan@Jhonatans-MacBook-Pro.local>
2025-02-20 12:12:54 -0500

    Update server URL

--- app/lib/constants/server-url.constant.ts
+++ app/lib/constants/server-url.constant.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://localhost:8080/api';
-// export const SERVER_URL = 'http://3.129.172.159:8080/api';
+// export const SERVER_URL = 'http://localhost:8080/api';
+export const SERVER_URL = 'http://3.129.172.159:8080/api';
